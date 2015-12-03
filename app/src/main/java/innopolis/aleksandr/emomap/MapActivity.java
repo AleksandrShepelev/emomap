e236eb1cfe092829e4af30d8666f1491d011e975
Aleksandr Shepelev <a.shepelev@innopolis.ru>
2015-12-04 00:08:11 +0300

    bloody location

--- app/src/main/java/innopolis/aleksandr/emomap/MapActivity.java
+++ app/src/main/java/innopolis/aleksandr/emomap/MapActivity.java
@@ -1,7 +1,16 @@
 package innopolis.aleksandr.emomap;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,19 +21,55 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "DBG";
     private GoogleMap mMap;
-
+    private LocationManager locationManager;
+    private Location currentLoc;
+    private Button locButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        locButton = (Button)findViewById(R.id.location_button);
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        boolean enabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!enabled) {
+            Log.d(TAG, "onCreate: gps disabled");
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                Log.d(TAG, "onClick: click!!");
+                double myLat = currentLoc.getLatitude();
+                double myLng = currentLoc.getLongitude();
+                LatLng currentLoc = new LatLng(myLat, myLng);
+                //   mMap.addMarker(new MarkerOptions().position(currentLoc).title("Marker in Sydney"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
+            }
+
+        });
+
     }
 
+    private void FindCurrentLocation() {
+        currentLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
+        if (currentLoc == null) {
+            Log.d(TAG, "onMapReady: gps null");
+            currentLoc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -37,10 +82,17 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        FindCurrentLocation();
+        if (currentLoc != null) {
+            double myLat = currentLoc.getLatitude();
+            double myLng = currentLoc.getLongitude();
+            // Add a marker in Sydney and move the camera
+            LatLng currentLoc = new LatLng(myLat, myLng);
+//            mMap.addMarker(new MarkerOptions().position(currentLoc).title("Marker in Sydney"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
+            Log.d(TAG, "onMapReady: network not null");
+        }
     }
+
+
 }
